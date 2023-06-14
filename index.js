79d4ea9397f0fd1ab48d167ee9bc239b9241f6f2
LKiok <kiokletia720@gmail.com>
2023-06-14 12:41:07 +0300

    done assignment

---
+++ index.js
@@ -0,0 +1,15 @@
+
+function divide( num1 = 2000,num2 = 100){
+    let division = num1 / num2
+    return division;
+}
+
+const square=(num4=5)=>{
+    let squared = num4*num4
+    return squared;
+}
+
+const add=(num5 = 100,num6 = 200, addition)=>{
+     addition = num5 + num6
+    return addition;
+}
